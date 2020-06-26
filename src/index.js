4b33a391f97dd6df408698ceba41c071f4e6fe0f
한소희 <hsh8616@naver.com>
2020-06-27 00:31:22 +0900

    주민등록번호 유효성 검사

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import "./index.scss";
 window.onload = function () {
   const inputName = document.getElementById("input-name");
   inputName.addEventListener("keyup", checkInputName);
+
+  const inputIdNumber = document.getElementById("input-id-number");
+  inputIdNumber.onkeyup = function () {
+    this.value = checkInputIdNumber(this.value);
+  };
 };
 
 const check_num = /[0-9]/; // 숫자
@@ -35,6 +40,19 @@ function checkInputName() {
   } else {
     validationError(inputGroup);
     return false;
+  }
+}
+
+// 주민등록번호
+function checkInputIdNumber(value) {
+  // 숫자만 입력 가능
+  value = value.replace(/[^0-9]/g, "");
+
+  // format 적용 (생년월일6자리-뒷자리1자리)
+  if (value.length < 7) {
+    return value;
+  } else {
+    return `${value.substr(0, 6)}-${value.substr(6)}`;
   }
 }
 
